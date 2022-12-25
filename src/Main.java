b0945e6bc491eb359d3f19f4a224b57aaee5718a
МАРК <markpan07@mail.ru>
2022-12-25 18:52:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println();
+        task2();
+        System.out.println();
+        task3();
+        System.out.println();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte a = (byte) 127;
+        short b = 30700;
+        int c = 500700;
+        long d = 111222333444L;
+        float e = 1.5f;
+        double f = 1.35;
+
+        System.out.println("Значение переменной с типом byte = " + a);
+        System.out.println("Значение переменной с типом short = " + b);
+        System.out.println("Значение переменной с типом int = " + c);
+        System.out.println("Значение переменной с типом long = " + d);
+        System.out.println("Значение переменной с типом float = " + e);
+        System.out.println("Значение переменной с типом double = " + f);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        float e = 27.12f;
+        long d = 987678965549L;
+        float q = 2.76f;
+        short s = 569;
+        int a = -159;
+        int b = 27897;
+        byte c = (byte) 67;
+
+
+
+        System.out.println(e);
+        System.out.println(d);
+        System.out.println(q);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 1");
+        byte a = (byte) 127;
+        short b = 30700;
+        int c = 500700;
+        long d = 111222333444L;
+        float e = 1.5f;
+        double f = 1.35;
+
+        System.out.println("Значение переменной с типом byte = " + a);
+        System.out.println("Значение переменной с типом short = " + b);
+        System.out.println("Значение переменной с типом int = " + c);
+        System.out.println("Значение переменной с типом long = " + d);
+        System.out.println("Значение переменной с типом float = " + e);
+        System.out.println("Значение переменной с типом double = " + f);
+    }
+}
